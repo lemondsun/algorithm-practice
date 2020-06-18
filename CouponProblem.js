b67e6be4997fff8d297ecd28a1b0a7736d3e89b9
Jason Mullings <wjasonmullings@gmail.com>
2020-06-18 09:48:02 -0400

    Fixed issue if dates are the same and refactored.

--- CouponProblem.js
+++ CouponProblem.js
@@ -1,9 +1,5 @@
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
-  if (enteredCode === correctCode && Date.parse(currentDate) < Date.parse(expirationDate))
-  return  true 
-  else
-    return false
-
+  return enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)
 }
 
 console.log(checkCoupon('123','123','September 5, 2014','October 1, 2014'))
