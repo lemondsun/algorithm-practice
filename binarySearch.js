0c5702fe370ab2775c69c781afbd58a39af256dc
Jason Mullings <wjasonmullings@gmail.com>
2022-05-17 22:26:32 -0400

    updated binary search

--- binarySearch.js
+++ binarySearch.js
@@ -1,25 +1,40 @@
 let binarySearch = (array, value) => {
-  let ans = []
+  // let ans = []
 
-  let divideAndCheck = (theArray) => {
-    let variableLeft = []
-    let variableRight = []
+  // let divideAndCheck = (theArray) => {
+  //   let variableLeft = []
+  //   let variableRight = []
  
-    array.map((x, i) => {
-      i <= Math.round(theArray.length / 2) ?
-        variableLeft.push(x)
-        :
-        variableRight.push(x)
-    })
+  //   array.map((x, i) => {
+  //     i <= Math.round(theArray.length / 2) ?
+  //       variableLeft.push(x)
+  //       :
+  //       variableRight.push(x)
+  //   })
 
-    if (variableLeft[variableLeft.length - 1] === value)
-    {
-      ans.push(variableLeft[variableLeft.length - 1]) 
-      return;
-    }
-    variableLeft[variableLeft.length - 1] < value ? 
-      divideAndCheck(variableRight)
+  //   if (variableLeft[variableLeft.length - 1] === value)
+  //   {
+  //     ans.push(variableLeft[variableLeft.length - 1])
+  //     return;
+  //   }
+  //   variableLeft[variableLeft.length - 1] < value ?
+  //     divideAndCheck(variableRight)
+  //     :
+  //     divideAndCheck(variableLeft)
+  // }
+  
+  let left = 0;
+  let right = array.length - 1;
+  let middle = Math.floor(array.length / 2);
+
+  while (left < right && array[middle] !== value) {
+    value < array[middle] ?
+      right = middle - 1
       :
-      divideAndCheck(variableLeft)
+      left = middle + 1
+    middle = Math.floor((left + right) / 2);
   }
-}
+  return array[middle] === value ? middle : -1;
+};
+
+console.log(binarySearch([1,2,3,4,5],2))
